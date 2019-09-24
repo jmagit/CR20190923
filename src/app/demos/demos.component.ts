77eb3f16ab5827d0d0d3464bcb584794507756a7
Javier <jmartin@grupoloyal.com>
2019-09-24 11:29:35 +0200

    Clase

--- src/app/demos/demos.component.ts
+++ src/app/demos/demos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationService } from '../common-app';
 
 @Component({
   selector: 'app-demos',
@@ -6,8 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./demos.component.css']
 })
 export class DemosComponent implements OnInit {
+  nombre: string = 'mundo';
+  idProvincia: number = 2;
+  listado = [
+    {id: 1, nombre: 'Toledo'},
+    {id: 2, nombre: 'CIUDAD REAL'},
+    {id: 3, nombre: 'cuenca'},
+    {id: 4, nombre: 'A coruñA'},
+  ];
 
-  constructor() { }
+  resultado: string = null;
+  visible = true;
+  estetica = { importante: true, error: false, urgente: true };
+
+  constructor(public notify: NotificationService) { }
+
+  saluda() {
+    this.resultado = `Hola ${this.nombre}`;
+  }
+  despide() {
+    this.resultado = `Adios ${this.nombre}`;
+  }
+  di(algo: string) {
+    this.resultado = `Dice ${algo}`;
+  }
+
+  cambia() {
+    this.visible = !this.visible;
+    this.estetica.error = !this.estetica.error;
+    this.estetica.importante = !this.estetica.importante;
+  }
+
+  calcula(a: number, b: number): number {
+    return a + b;
+  }
+
+  public add(nombre: string) {
+    const id = this.listado.length > 0 ? this.listado[this.listado.length - 1].id + 1 : 1;
+    this.listado.push({id, nombre});
+    this.idProvincia = id;
+  }
+
 
   ngOnInit() {
   }

1e38e701cd2c4df3c05c7eaa68634cbe79c75f63
Javier <jmartin@grupoloyal.com>
2019-09-24 16:14:50 +0200

    vista notificaciones

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,12 +1,13 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from '../main/home/home.component';
+import { NotificationComponent } from '../main/notification/notification.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
-  exports: [HomeComponent],
+  declarations: [HomeComponent, NotificationComponent],
+  exports: [HomeComponent, NotificationComponent],
   imports: [
     CommonModule
   ]
